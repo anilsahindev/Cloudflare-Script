9b736b93c625ef65636b035702139ca97ec0dff3
ANIL ŞAHİN <anilsahindev@gmail.com>
2020-11-30 14:39:15 +0300

    ı need this

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cloudflare-Script
+
+If you want to bulk delete dns records on cloudflare use this script.
---
+++ index.js
@@ -0,0 +1,17 @@
+const settings = {
+    email: 'your@email',
+    key: 'your_api_key',
+    domain: 'domain.example'
+}
+
+(async() => {
+    const cloudflare = require('cloudflare')({email: settings.email, key: settings.key});
+    const zone_id = await cloudflare.zones.browse().then(zones => zones.result.find(x => x.name === settings.domain).id);
+    const info = await cloudflare.dnsRecords.browse(zone_id).then(x => x.result_info);
+    console.log(`Pages to delete: ${info.total_pages}`);
+    for (let i=0;i<info.total_pages;i++) {
+        const dns_records = await cloudflare.dnsRecords.browse(zone_id).then(x => x.result.map(dns_record => dns_record.id));
+        await Promise.all(dns_records.map(id => cloudflare.dnsRecords.del(zone_id, id)));
+        console.log(`Page ${i+1} is deleted remained ${info.total_pages-i} pages`);            
+    }
+})();
